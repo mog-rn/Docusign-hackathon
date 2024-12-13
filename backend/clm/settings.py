c91dc57857574333f6ddc3183b55fec3cda43f36
Karen Miano <mianokaren1798@gmail.com>
2024-12-13 20:32:48 +0300

    Add database model for User, Organization and Domain

--- backend/clm/settings.py
+++ backend/clm/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
+    'authentication',
+    'organizations',
+    'contracts',
 ]
+
+AUTH_USER_MODEL = 'core.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
