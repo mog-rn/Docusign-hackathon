c6075dfe7fbff6826b4e4da3f2b27057fd992e5e
Karen Miano <mianokaren1798@gmail.com>
2025-01-21 08:01:42 +0300

    Stop tracking logs directory

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -1,24 +1,27 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.exceptions import PermissionDenied
 
+
 class IsMainAdmin(BasePermission):
     """
     Allows access only to the django superuser.
     """
+
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_superuser
-    
+
 
 class IsOrganizationAdmin(BasePermission):
     """
     Allows access only to users who are organization admins.
     """
+
     def has_permission(self, request, view):
         user = request.user
         is_organization_admin = user.roles.filter(
-            role__name='admin',
-            role__organization=user.organization
+            role__name="admin", role__organization=user.organization
         ).exists()
+        print(is_organization_admin)
         return is_organization_admin
 
 
@@ -26,6 +29,7 @@ class IsInOrganization(BasePermission):
     """
     Allows access to users who belong to the requested organization.
     """
+
     def has_permission(self, request, view):
-        organization_id = view.kwargs.get('organizationId')
+        organization_id = view.kwargs.get("organizationId")
         return request.user.organization.id == organization_id
