7ac35569d683b6539ae27e97e98b92c602c57b0f
mog-rn <mogaka.amo254@gmail.com>
2025-01-21 14:47:24 +0300

    fix(backend): migrations issues

--- backend/core/migrations/0002_initial.py
+++ backend/core/migrations/0002_initial.py
@@ -11,7 +11,6 @@ class Migration(migrations.Migration):
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
         ('core', '0001_initial'),
-        ('organizations', '0001_initial'),
     ]
 
     operations = [
