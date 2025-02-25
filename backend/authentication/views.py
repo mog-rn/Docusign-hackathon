c3dc14210a98166627675905c294894a0ed8fb4d
mog-rn <mogaka.amo254@gmail.com>
2025-02-25 04:07:43 +0300

    trigger workflow

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -158,6 +158,3 @@ class AcceptInvitationView(generics.GenericAPIView):
             return Response(response_data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
