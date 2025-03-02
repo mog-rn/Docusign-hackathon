2981773397460110e134db94491932a7e40f5aed
Karen Miano <mianokaren1798@gmail.com>
2025-03-02 10:54:25 +0300

    feat(backend): Refined endpoint for sending contracts for signing with better status codes and error handling; created endpoint for registering users as signatureAPI senders

--- backend/esignature/utils.py
+++ backend/esignature/utils.py
@@ -2,34 +2,30 @@ from contracts.services.s3 import S3
 
 
 def prepare_envelope_data(sender, contract, data_from_request):
+    """
+    Build complete envelope payload for signatureAPI using data from the frontend
+    plus the contract file from s3.
+    """
+
     # generate presigned download url for signatureAPI to source document from
     s3_client = S3()
     contract_url = s3_client.generate_presigned_url_expanded(
         "get_object", contract.file_path
     )
 
-    recipients = data_from_request["recipients"]
-    for recipient in recipients:
-        recipient["type"] = recipient.pop("recipient_type")
-
-    places = data_from_request["places"]
-    for place in places:
-        place["type"] = place.pop("place_type")
-
-    # Make the subject and message configurable
     envelope_data = {
         "title": contract.title,
-        "message": data_from_request["message"],
+        "message": "Please review the agreement at your convenience and provide your electronic signature.",
         "routing": data_from_request["routing"],
         "documents": [
             {
                 "title": contract.title,
                 "url": contract_url,
-                "places": places,
-                "format": data_from_request["document_format"]
+                "places": data_from_request["places"],
+                "format": data_from_request["document_format"],
             },
         ],
-        "recipients": recipients,
+        "recipients": data_from_request["recipients"],
         "sender": {
             "email": sender.email,
             "name": f"{sender.first_name} {sender.last_name}",
